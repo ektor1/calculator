76dbf05827c16ebd76c451f6ef331653c010a405
ektor1 <hc.nlks@gmail.com>
2024-09-25 16:10:14 +0100

    first commit

---
+++ functions.js
@@ -0,0 +1,19 @@
+// Calculator functions
+
+let number1;
+let number2;
+let operator;
+
+// Addition
+
+
+// Subtraction
+
+
+// Division
+
+
+// Multiplication
+
+
+// Operate function takes an operator, 2 numbers, and calls one of the functions above
