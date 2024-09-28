479d574b31e1ec1ff33b203cc6457fe01ccdb68e
ektor1 <hc.nlks@gmail.com>
2024-09-28 20:29:19 +0100

    Display numbers on screen

--- functions.js
+++ functions.js
@@ -1,36 +1,41 @@
 // Calculator functions
-
-// Variables to update display
-let num1;
-let num2;
-let operator;
-
-
 function add(num1, num2)
 {
     return num1 + num2;
 }
-
 
 function subtract(num1, num2)
 {
     return num1 - num2;
 }
 
-
 function divide(num1, num2)
 {
     return num1 / num2;
 }
-
 
 function multiply(num1, num2)
 {
     return num1 * num2;
 }
 
-
-function operate(operator, num1, num2)
+function calculate(operation, num1, num2)
 {
-    return operator(num1, num2);
+    return operation(num1, num2);
+}
+
+
+let firstNumber, secondNumber;
+let operator;
+let upperDisplayText = document.getElementById("upperDisplay");
+let lowerDisplayText = document.getElementById("lowerDisplay");
+
+function appendToDisplay(input)
+{
+    lowerDisplayText.innerText += input;
+}
+
+function clearDisplay()
+{
+    lowerDisplayText.innerText = "";
 }
