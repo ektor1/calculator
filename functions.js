f34e30fb78e5d48d78efd4a4fcb81004e285a27f
ektor1 <hc.nlks@gmail.com>
2024-09-29 23:30:39 +0100

    Add calcs functionality

--- functions.js
+++ functions.js
@@ -1,41 +1,71 @@
 // Calculator functions
-function add(num1, num2)
-{
-    return num1 + num2;
-}
-
-function subtract(num1, num2)
-{
-    return num1 - num2;
-}
-
-function divide(num1, num2)
-{
-    return num1 / num2;
-}
-
-function multiply(num1, num2)
-{
-    return num1 * num2;
-}
-
-function calculate(operation, num1, num2)
-{
-    return operation(num1, num2);
-}
+let add = (firstNumber, secondNumber) => firstNumber + secondNumber;
+let subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
+let divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
+let multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+let calculate = (operator, firstNumber, secondNumber) => operator(parseInt(firstNumber), parseInt(secondNumber));
 
 
-let firstNumber, secondNumber;
+// Map operator signs to functions
+let map = {};
+map['+'] = add;
+map['-'] = subtract;
+map['x'] = multiply;
+map['/'] = divide;
+
+
+// Create variables for calculation
+let firstNumber, secondNumber, round;
 let operator;
-let upperDisplayText = document.getElementById("upperDisplay");
-let lowerDisplayText = document.getElementById("lowerDisplay");
+const upperDisplayText = document.getElementById("upperDisplay");
+const lowerDisplayText = document.getElementById("lowerDisplay");
 
-function appendToDisplay(input)
+
+// Append single digit in the lower display
+function appendDigit(input)
 {
     lowerDisplayText.innerText += input;
 }
 
+
+// Append first number and operation selected to the upper display
+function appendOperator(operatorInput)
+{
+    firstNumber = lowerDisplayText.innerText;
+    operator = operatorInput;
+    upperDisplayText.innerText = lowerDisplayText.innerText + " " + operator;
+    lowerDisplayText.innerText = "";
+}
+
+
+// Append second number to the upper display and show result of calculation on the lower display
+function equals()
+{
+    secondNumber = lowerDisplayText.innerText;
+    upperDisplayText.innerText += " " + lowerDisplayText.innerText;
+
+    lowerDisplayText.innerText = calculate(map[operator], firstNumber, secondNumber);
+    
+}
+
+
 function clearDisplay()
 {
+    upperDisplayText.innerText = "";
     lowerDisplayText.innerText = "";
+    operator = undefined;
+    firstNumber = undefined;
+    secondNumber = undefined;
+}
+
+
+function switchPercentage()
+{
+    lowerDisplayText.innerText = (lowerDisplayText.innerText / 100).toString();
+}
+
+
+function switchSign()
+{
+    lowerDisplayText.innerText = (-lowerDisplayText.innerText).toString();
 }
